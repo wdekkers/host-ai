e4913afa0259d8fad1aba3d718d51b1016d5f0bb
Wesley Dekkers <wesley@wd-media.nl>
2026-03-23 10:08:33 -0500

    fix(cdn): use namespace import for React (NodeNext compat)

--- packages/cdn/src/CdnImage.tsx
+++ packages/cdn/src/CdnImage.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import * as React from "react";
 import { buildImageUrl, buildSrcSet, type ImageParams } from "./utils.js";
 
 export interface CdnImageProps
